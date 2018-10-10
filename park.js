3c71062f739f45669ba7a0a2c532b03e6131d805
Hadsan Geele <hgeele22@gmail.com>
2018-10-10 09:13:29 +0100

    test most visited, test passed

--- park.js
+++ park.js
@@ -13,7 +13,31 @@ Park.prototype.addDinosaur = function (dinosaur) {
 };
 
 Park.prototype.removeDinosaur = function (dinosaur) {
-  let position = this.dinosaurs.indexOf(dinosaur)
-    return this.dinosaurs.splice(position, 1);
+  let indexPosition = this.dinosaurs.indexOf(dinosaur)
+    return this.dinosaurs.splice(indexPosition, 1);
 };
+
+Park.prototype.mostVisited = function () {
+   //assigned the first object/dinasour to a new variable.
+  let mostVisited = this.dinosaurs[2]
+  //loop over all the objects/dinosaurs in the array
+  for(dinosaur of this.dinosaurs){
+    //find out what the number attraction number and
+    // check that against the current objects/dinasour attraction number that is in the variable above.
+    if(dinosaur.visitorAttractedPerDay > mostVisited.visitorAttractedPerDay){
+        //if it is more then the variable changes to that object.
+    mostVisited = dinosaur;
+  }
+    //return what the most popular object/dinosaur is.
+  return mostVisited
+}
+
+};
+
+//proto type spiciestypesize = function(spiciestype)
+// let spiciestypesizeArray = [] ...empty, starts empty
+// for loop all the dinosaur in the array
+// if statement: if the dinosaur.speciesType is equal === spiciestype
+//speciesTypeSizeArray.push(dinosaur) ..the dinasour that are similar species
+// return speciesTypeSizeArray
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -16,7 +16,7 @@ describe('Park', function() {
 
   beforeEach(function(){
     park = new Park("Dino_Park", 10 );
-    dinosaur = new Dinosaur("T-rex", "carnivore", 30 );
+    dino1 = new Dinosaur("T-rex", "carnivore", 30 );
     dino2 = new Dinosaur("Mammut", "herbivore", 10)
     dino3 = new Dinosaur("Compsognathus" , "carnivore", 5)
   });
@@ -28,7 +28,7 @@ describe('Park', function() {
   it("should have a ticket price", function(){
     assert.strictEqual(park.price, 10);
   });
- // starts with empty  array [] of dinosaurs
+  // starts with empty  array [] of dinosaurs
   describe("dinosaurs", function(){
     it("should start with no dinosaurs", function(){
       const actual = park.dinosaurs;
@@ -36,25 +36,36 @@ describe('Park', function() {
     });
 
     it('should be able to return the number of dinosaurs', function(){
-       const actual = park.numberOfDinosaurs()
-       assert.strictEqual(actual, 0)
-     })
-     // add dinosaur
-     it('should be able to add a dinosaur to its collection of dinosaurs ', function(){
-        park.addDinosaur(dino2)
-        const actual = park.numberOfDinosaurs()
-        assert.strictEqual(actual, 1)
-      })
+      const actual = park.numberOfDinosaurs()
+      assert.strictEqual(actual, 0)
+    });
+    // add dinosaur
+    it('should be able to add a dinosaur to its collection of dinosaurs ', function(){
+      park.addDinosaur(dino2);
+      const actual = park.numberOfDinosaurs()
+      assert.strictEqual(actual, 1)
+    });
 
-      // remove dinosaur
-      it('should be able to Remove a dinosaur from its collection of dinosaurs ', function(){
-         park.addDinosaur(dino2)
-         park.addDinosaur(dino3)
-         park.removeDinosaur(dino2)
-         const actual = [dino3]
-         const expected = park.dinosaurs
-         assert.deepStrictEqual(actual, expected)
-       })
+    // remove dinosaur
+    it('should be able to Remove a dinosaur from its collection of dinosaurs ', function(){
+      park.addDinosaur(dino2);
+      park.addDinosaur(dino3);
+      park.removeDinosaur(dino2)
+      const actual = [dino3]
+      const expected = park.dinosaurs
+      assert.deepStrictEqual(actual, expected)
+    });
+    // Find the dinosaur that attracts the most visitors
+    it("should be able to find the dinosaur that attracts the most visitors", function(){
+      park.addDinosaur(dino2);
+      park.addDinosaur(dino3);
+      park.addDinosaur(dino1);
+      assert.strictEqual(park.mostVisited().speciesType, "T-rex" )
+    });
+
+    // it should find dinosaur of particular speciesType
+    // add all dinosaur and make sure that there are similar species to the list of dinosaurs
+    // asert park.spiciestypesize(the speciestype most similar).length, number of similar speciestype
 
   });
 });
